976f7f2da5c4cd443c66355b4d8af61d31b6920f
Phat Luc <phatluc1212@gmail.com>
2018-09-30 23:32:31 -0700

    Working infinite scroll handling.

--- main.py
+++ main.py
@@ -6,32 +6,92 @@ turo_sfo = "https://turo.com/search?airportCode=SFO&endDate=10%2F07%2F2018&endTi
 page = requests.get(turo_sfo)
 
 browser = webdriver.Chrome()
+default_size = browser.get_window_size()
+browser.set_window_size(2000, default_size['height'])
+
 browser.get(turo_sfo)
-time.sleep(5)
-page = browser.page_source
-# browser.close()
+time.sleep(10)
 
-soup = BeautifulSoup(page, 'html.parser')
-# print (soup.prettify())
-list_1 = soup.findAll("a", {"class":"vehicleCard"})
-if (len(list_1) == 0):
-    list_1 = soup.findAll("a", {"class":"vehicleWithDetails searchResult"})
-if (len(list_1) == 0):
-    print("Cannot fetch list of results")
-    sys.exit(0)
-for element in list_1:
-    print("https://turo.com"+element['href'])
-    # print(element)
-# for a_link in soup.findAll("a", {"class":"vehicleCard"}):    
-#     print(a_link)
+def handleSingleCardFormat():
+    print ("handleSingleCard called")
+    try:
+        # Test whether it's single card format
+        browser.find_element_by_css_selector('a.vehicleWithDetails.searchResult')
+        # Fetch all a tags
+        links = browser.find_elements_by_css_selector('a.vehicleWithDetails.searchResult')
+        if (len(links) == 0):
+            print ("list is empty")
+            return
+        for each in links:
+            print (each.get_attribute('href'))
+        # scroll to last card on page
+        browser.execute_script("arguments[0].scrollIntoView();", links[-1])
+        time.sleep(1)
+        # get next batch
+        links = browser.find_elements_by_css_selector('a.vehicleWithDetails.searchResult')
+        print (len(links))
+        for each in links:
+            print (each.get_attribute('href'))
+    except:
+        raise LookupError('a.vehicleWithDetails.searchResult')
+
+def handleMultipleCardsFormat():
+    print ("handleMultipleCards called")
+    try:
+        browser.find_element_by_css_selector('a.vehicleCard')
+        # Fetch all a tags
+        links = browser.find_elements_by_css_selector('a.vehicleCard')
+        if (len(links) == 0):
+            print ("list is empty")
+            return
+        for each in links:
+            print (each.get_attribute('href'))
+        # scroll to last card on page
+        browser.execute_script("arguments[0].scrollIntoView();", links[-1])
+        time.sleep(1)
+        # get next batch
+        new_links = browser.find_elements_by_css_selector('a.vehicleCard')
+        print (len(new_links))
+        for each in new_links:
+            print (each.get_attribute('href'))
+    except:
+        raise LookupError('a.vehicleCard')
+
+# Determine which format being return by turo
+try:
+    handleSingleCardFormat()
+except:
+    print ("Trying option 2..")
+    try:        
+        handleMultipleCardsFormat()
+    except:
+        print ("Both options fail. Terminating..")
+        browser.quit()
+        sys.exit(0)
+# browser.quit()
 
 
+# if (len(links) == 0):
+#     print("Executing option 2...")
+#     links = browser.find_elements_by_css_selector("a.vehicleCard")
+# if (len(links) == 0):
+#     print ("Both options fail. Terminating...")
+#     sys.exit(0)
+# print (len(links))
+# for each in links:
+#     print(each.get_attribute("href"))
 
-# print(soup.prettify())
+# scroll to last card on page
+# browser.execute_script("arguments[0].scrollIntoView();", links[-1])
 
-# outfile = open('out.txt', 'w')
-# print len(soup.find_all('span', attrs={'class':'sx-price-whole'}))
-# print len(soup.find_all('h2'))
-# print soup.find('div', attrs={"class":"searchResultsList"})
-# for each in soup.find_all('h2'):
-#     print each.string
+
+# page = browser.page_source
+# soup = BeautifulSoup(page, 'html.parser')
+# list_1 = soup.findAll("a", {"class":"vehicleCard"})
+# if (len(list_1) == 0):
+#     list_1 = soup.findAll("a", {"class":"vehicleWithDetails searchResult"})
+# if (len(list_1) == 0):
+#     print("Cannot fetch list of results")
+#     sys.exit(0)
+# for element in list_1:
+#     print("https://turo.com"+element['href'])
